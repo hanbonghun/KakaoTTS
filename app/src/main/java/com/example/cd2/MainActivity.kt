d919d21e4f56e45cd7b03f1f6e4a8f7e11262ca2
hanbonghun <bng4535@khu.ac.kr>
2022-10-04 19:59:55 +0900

    오디오 조절

--- app/src/main/java/com/example/cd2/MainActivity.kt
+++ app/src/main/java/com/example/cd2/MainActivity.kt
@@ -6,13 +6,16 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.*
 import android.content.pm.PackageManager
+import android.media.AudioManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.support.annotation.RequiresApi
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
 import android.view.View
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.ToggleButton
@@ -20,8 +23,20 @@ import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
 
+//    var volumeBroadcastReceiver = object: BroadcastReceiver(){
+//        override fun onReceive(p0: Context?, p1: Intent?) {
+//            var action = intent?.getAction()
+//
+//            if (action != null) {
+//                if(action.equals("android.media.VOLUME_CHANGED_ACTION")){
+//                    System.out.println("volume changed")
+//                }
+//
+//            }
+//        }
+//    }
+    //블루투스 리시버
     val bluetoothBroadcastReceiver = object : BroadcastReceiver() {
-        @RequiresApi(Build.VERSION_CODES.M)
         override fun onReceive(contxt: Context?, intent: Intent?) {
             val bleOnOffBtn:ToggleButton = findViewById(R.id.bluetooth_on_off_btn)
 
@@ -41,9 +56,23 @@ class MainActivity : AppCompatActivity() {
             }
             if(BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)){
                 val device: BluetoothDevice? = intent?.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE) //연결된 장치
+
                 bleOnOffBtn.isChecked = true
                 var bleDeviceTextView : TextView= findViewById(R.id.connectedDevice)
-                    bleDeviceTextView.setText("connected")
+                if (ActivityCompat.checkSelfPermission(
+                        this@MainActivity,
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.BLUETOOTH_CONNECT),1)
+                    return
+                }
+                if (device != null) {
+                    bleDeviceTextView.setText(device.name)
+                }
+
                 Toast.makeText(
                     baseContext,
                     "Device is now Connected",
@@ -52,6 +81,9 @@ class MainActivity : AppCompatActivity() {
                 System.out.println(device)
             }else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
                 bleOnOffBtn.isChecked = false
+                var bleDeviceTextView : TextView= findViewById(R.id.connectedDevice)
+
+                bleDeviceTextView.setText("")
 
                 System.out.println("해제")
                 Toast.makeText(
@@ -62,9 +94,28 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val audioManager = this.applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val volumeSeekBar:SeekBar = findViewById(R.id.volumeSeeBar)
+        var currentVolume =audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)  //현재 음량
+        var maxVolume =audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+
+        volumeSeekBar.max =maxVolume
+        volumeSeekBar.setProgress(currentVolume)
+
+        volumeSeekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
 
 
         val filter = IntentFilter().apply{
@@ -74,8 +125,21 @@ class MainActivity : AppCompatActivity() {
             addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED)
         }
         registerReceiver(bluetoothBroadcastReceiver,filter)
-
     }
+
+//    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+//        val audioManager = this.applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+//        val volumeSeekBar:SeekBar = findViewById(R.id.volumeSeeBar)
+//        var currentVolume =audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)  //현재 음량
+//
+//        when (keyCode) {
+//            KeyEvent.KEYCODE_VOLUME_DOWN,KeyEvent.KEYCODE_VOLUME_UP -> {
+//                volumeSeekBar.setProgress(currentVolume)
+//            }
+//        }
+//        return super.onKeyDown(keyCode, event)
+//    }
+
 
 
     fun isBluetoothEnabled(): Boolean {
@@ -107,11 +171,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     override fun onStart() {
+        val bleOnOffBtn:ToggleButton = findViewById(R.id.bluetooth_on_off_btn)
 
         super.onStart()
 
         val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
         if(Build.VERSION.SDK_INT >= 23){
             if(!notificationManager.isNotificationPolicyAccessGranted){ //알림 접근 권한이 허용 여부 확인
                 this.startActivity(Intent(android.provider.Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
@@ -124,7 +191,23 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
-    }
 
+        //블루투스 켜짐/꺼짐에 따라 버튼 on/off
+        if(!isBluetoothEnabled()){
+            bleOnOffBtn.isChecked = false
+        }else{
+            bleOnOffBtn.isChecked = true
+        }
+
+        //초기 볼륨
+        val audioManager = this.applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val volumeSeekBar:SeekBar = findViewById(R.id.volumeSeeBar)
+        var currentVolume =audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)  //현재 음량
+        var maxVolume =audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+
+        volumeSeekBar.max =maxVolume
+        volumeSeekBar.setProgress(currentVolume)
+
+    }
 
 }
