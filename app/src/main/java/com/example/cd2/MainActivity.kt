179d3e4f17530203d738e415ab5ba99a33025687
gurum505 <sayhojeong505@gmail.com>
2022-10-09 20:40:31 +0900

    TTS 추가, 블루투스 연결시 음성 나오는 것 까지 확인 미구현: speed조절,Engine고르기

--- app/src/main/java/com/example/cd2/MainActivity.kt
+++ app/src/main/java/com/example/cd2/MainActivity.kt
@@ -14,11 +14,8 @@ import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.KeyEvent
 import android.view.View
-import android.widget.SeekBar
+import android.widget.*
 import android.widget.SeekBar.OnSeekBarChangeListener
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.ToggleButton
 
 
 class MainActivity : AppCompatActivity() {
@@ -35,16 +32,22 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        }
 //    }
+
+    var speedRate:Float = 1.0F
+
+
     //블루투스 리시버
-    val bluetoothBroadcastReceiver = object : BroadcastReceiver() {
+    //http://jinyongjeong.github.io/2018/09/27/bluetoothpairing/
+    //https://jung-max.github.io/2019/08/27/Android-Bluetooth/
+    private val bluetoothBroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(contxt: Context?, intent: Intent?) {
             val bleOnOffBtn:ToggleButton = findViewById(R.id.bluetooth_on_off_btn)
 
-            var action = intent?.getAction()
+            var action = intent?.action //입력된 action
 
             if(action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
                 var state =
-                    intent?.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                    intent?.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)
                 if (state == BluetoothAdapter.STATE_OFF) {
                     System.out.println("블루투스 off")
                     bleOnOffBtn.isChecked = false
@@ -65,25 +68,37 @@ class MainActivity : AppCompatActivity() {
                     ) != PackageManager.PERMISSION_GRANTED
                 ) {
                     // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
+                    //       ActivityCompat#requestPermissions
                     ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.BLUETOOTH_CONNECT),1)
                     return
                 }
                 if (device != null) {
-                    bleDeviceTextView.setText(device.name)
+                    bleDeviceTextView.text = device.name
+                    //bleDeviceTextView.text = device.name
+
+                    Toast.makeText(
+                        baseContext,
+                        "Device is now Connected",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }else{
+
+                    Toast.makeText(
+                        baseContext,
+                        "No Device is Connected",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return
                 }
 
-                Toast.makeText(
-                    baseContext,
-                    "Device is now Connected",
-                    Toast.LENGTH_SHORT
-                ).show()
-                System.out.println(device)
+                //Device와 TTS audio 연결
+                device.createBond()
+
             }else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
                 bleOnOffBtn.isChecked = false
                 var bleDeviceTextView : TextView= findViewById(R.id.connectedDevice)
 
-                bleDeviceTextView.setText("")
+                bleDeviceTextView.text = ""
 
                 System.out.println("해제")
                 Toast.makeText(
@@ -105,7 +120,7 @@ class MainActivity : AppCompatActivity() {
         var maxVolume =audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
 
         volumeSeekBar.max =maxVolume
-        volumeSeekBar.setProgress(currentVolume)
+        volumeSeekBar.progress = currentVolume
 
         volumeSeekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
@@ -117,14 +132,53 @@ class MainActivity : AppCompatActivity() {
         })
 
 
-
+        //다른 어플이나 application에서 broadcast하는 메시지를 받을 수 있도록 설정
         val filter = IntentFilter().apply{
-            addAction(BluetoothAdapter.ACTION_STATE_CHANGED)
-            addAction(BluetoothDevice.ACTION_ACL_CONNECTED)
-            addAction(BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED)
-            addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED)
+            addAction(BluetoothAdapter.ACTION_STATE_CHANGED)//블루투스상태변화액션
+            addAction(BluetoothDevice.ACTION_ACL_CONNECTED)//연결확인
+            addAction(BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED)//
+            addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED)//연결끊김확인
         }
-        registerReceiver(bluetoothBroadcastReceiver,filter)
+        registerReceiver(bluetoothBroadcastReceiver,filter)//reiver와 filter연결
+
+        //https://www.geeksforgeeks.org/spinner-in-kotlin/
+        initSpeedRateSpinner()
+
+
+    }
+
+    private fun initSpeedRateSpinner(){
+        val speedList:Array<Float> = arrayOf(1.0F,2.0F,3.0F)
+        val speedRateSpinner = findViewById<Spinner>(R.id.spinner)
+        val speedAdapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,speedList)
+        speedRateSpinner.adapter=speedAdapter
+
+        //https://jeongupark-study-house.tistory.com/11
+        speedRateSpinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>,
+                                        view: View, position: Int, id: Long) {
+                //set speedRate of TTS
+                when(position){
+                    0->speedRate = 1.0F
+                    1->speedRate = 1.5F
+                    2->speedRate = 2.0F
+                }
+
+                //TODO: send speedRate to NotificationListener
+//                val intent = Intent(android.provider.Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
+//                intent.putExtra("SpeedRate",speedRate)
+//                startActivity(intent)
+
+                //show user speedRate
+                Toast.makeText(this@MainActivity,
+                    "speed set : $speedRate",Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
     }
 
 //    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
@@ -141,22 +195,19 @@ class MainActivity : AppCompatActivity() {
 //    }
 
 
-
+    //TODO: 한번켜지면 꺼지지 않음
     fun isBluetoothEnabled(): Boolean {
         val myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        if(myBluetoothAdapter.isEnabled()){
-            return true
-        }else{
-            return false
-        }
+        return myBluetoothAdapter.isEnabled
     }
 
     //블루투스 연결 설정 버튼 클릭 시
     fun onClickBluetoothBtn(view: View) {
-        val intent = Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
-        startActivity(intent); // 블루투스 연결 설정 화면으로 이동
+        val intent = Intent(Settings.ACTION_BLUETOOTH_SETTINGS)
+        startActivity(intent) // 블루투스 연결 설정 화면으로 이동
     }
 
+    //TODO: 한번켜지면 꺼지지 않음
     //알림 설정 버튼 클릭 시
     fun onClickNotiSettingBtn(view: View) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -179,25 +230,28 @@ class MainActivity : AppCompatActivity() {
 
         val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
+        //https://kobbi-reply.tistory.com/17
         if(Build.VERSION.SDK_INT >= 23){
             if(!notificationManager.isNotificationPolicyAccessGranted){ //알림 접근 권한이 허용 여부 확인
-                this.startActivity(Intent(android.provider.Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
+                val intent = Intent(android.provider.Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
+                this.startActivity(intent)
             }else{
-//      request permission 은 오직 한 번만
+                //request permission 은 오직 한 번만
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_DENIED||ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED) {
                     // Request the user to grant permission to read SMS messages
-                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS,Manifest.permission.BLUETOOTH_CONNECT), 2);
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS,Manifest.permission.BLUETOOTH_CONNECT), 2)
                 }
 
             }
         }
 
         //블루투스 켜짐/꺼짐에 따라 버튼 on/off
-        if(!isBluetoothEnabled()){
+        /*if(!isBluetoothEnabled()){
             bleOnOffBtn.isChecked = false
         }else{
             bleOnOffBtn.isChecked = true
-        }
+        }*/
+        bleOnOffBtn.isChecked = isBluetoothEnabled()
 
         //초기 볼륨
         val audioManager = this.applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
@@ -206,7 +260,10 @@ class MainActivity : AppCompatActivity() {
         var maxVolume =audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
 
         volumeSeekBar.max =maxVolume
-        volumeSeekBar.setProgress(currentVolume)
+        volumeSeekBar.progress = currentVolume
+
+        //TODO: 핸드폰 옆의 볼륨을 높힐때 바로 media 볼륨이 잡히지 않는다
+
 
     }
 
--- app/src/main/java/com/example/cd2/NotificationListener.kt
+++ app/src/main/java/com/example/cd2/NotificationListener.kt
@@ -7,26 +7,166 @@ import android.os.Bundle
 import android.os.IBinder
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.speech.tts.TextToSpeech
 import android.util.Log
+import android.widget.Toast
+import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NotificationListener : NotificationListenerService() {
+
+    private var tts: TextToSpeech? =null
 
 
     override fun onListenerConnected() {
         super.onListenerConnected()
+        initTextToSpeech()
     }
 
     override fun onNotificationPosted(sbn: StatusBarNotification) {
         Log.i("NotificationListener", " onNotificationPosted() - $sbn")
         Log.i("NotificationListener", " PackageName:" + sbn.packageName)
         Log.i("NotificationListener", " PostTime:" + sbn.postTime)
-        val notificatin: Notification = sbn.notification
-        val extras: Bundle = notificatin.extras
-        val title = extras.getString(Notification.EXTRA_TITLE)
+        val notification: Notification = sbn.notification
+        val extras: Bundle = notification.extras
+        val title= extras.getString(Notification.EXTRA_TITLE)
         val text = extras.getCharSequence(Notification.EXTRA_TEXT)
         val subText = extras.getCharSequence(Notification.EXTRA_SUB_TEXT)
         Log.i("NotificationListener", " Title:$title")
         Log.i("NotificationListener", " Text:$text")
         Log.i("NotificationListener", "Sub Text:$subText")
+        Log.i("NotificationListener", "Time:" + sbn.postTime)
+        Log.i("NotificationListener", "Time:" + sbn.user)
+
+        //https://m.blog.naver.com/yuyyulee/221531478175
+        //TODO: 발신자, 발신내용, 발신시간 switch에 따라 onOff
+        //TODO: 목소리 종류? https://stackoverflow.com/questions/9815245/android-text-to-speech-male-voice
+
+        //모든 notification을 잡기 때문에 필요한 것만 구분해야함
+        if(sbn.packageName != "com.kakao.talk" && sbn.packageName.indexOf("messaging") == -1) return
+
+        //TODO: application의 speedRate와 연결하기
+        tts?.setSpeechRate(1.0F)//1.0기본 float
+
+
+        if(title != null){
+            // title: 카카오톡방이름
+            //이모티콘(유니코드)무시 필요?
+            //발신자: title로 대체?
+            if(title.toString() == "나"){
+                return
+            }
+            speakTTS(title.toString())
+        }
+        if(text != null){
+            // 카카오톡 방 내용
+            //TODO: 사진, 동영상, 파일 무시 필요?
+            var t=text.toString()
+            if(t == "사진" || t == "동영상을 보냈습니다."){
+                return
+            }
+            //너무 긴 경우 무시
+            if(t.length >= 20){
+                t.substring(20)
+                t += "   이상입니다."
+            }
+            speakTTS(t)
+        }
+        //if(subText != null)speakTTS(subText.toString()) // "4개의 안읽은 메시지"
+
+        //날짜
+        //https://soft.plusblog.co.kr/62
+        //val sdf = SimpleDateFormat("HH시 MM분")
+        //val postTime = sdf.format(sbn.postTime)
+        //if(postTime != null)speakTTS(postTime.toString())
+
     }
+
+    private fun initTextToSpeech() {
+
+
+
+        tts = TextToSpeech(this) {
+            if (it == TextToSpeech.SUCCESS) {
+                val result = tts?.setLanguage(Locale.KOREA)
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Toast.makeText(this, "language not supported", Toast.LENGTH_SHORT).show()
+                    return@TextToSpeech
+                }
+
+            }
+        }
+
+        //TODO: change TTS Engine
+        //https://stackoverflow.com/questions/7362534/how-to-programmatically-change-tts-default-engine
+        //List<TextToSpeech.EngineInfo> engineInfoList = textToSpeech.getEngines()
+        //.setEngineByPackageName deprecated
+        //public TextToSpeech (Context context,
+        //                TextToSpeech.OnInitListener listener,
+        //                String engine)
+        //TODO: application onCreated할때 list를 spinner에 넣어줘야 함
+        //      선택시 set Engine
+        val engineList : List<TextToSpeech.EngineInfo> = tts!!.engines
+        Log.i("NotificationListener", "Engine: $engineList")
+//        EngineInfo{name=com.samsung.SMT}
+//        EngineInfo{name=com.google.android.tts}
+
+
+    }
+
+    private fun speakTTS(string:String){
+        if(string == null || string == ""){
+            Log.i("NotificationListener", "speakTTS:null")
+            return
+        }
+        tts?.speak(string,TextToSpeech.QUEUE_ADD,null,null)
+    }
+
+    //https://www.tutorialkart.com/kotlin-android/android-text-to-speech-kotlin-example/
+    override fun onDestroy() {
+        if(tts != null){
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
+    }
+//    2022-10-09 09:45:50.819 2790-2790/com.example.cd2 I/NotificationListener: Sub Text:null
+//    2022-10-09 09:45:53.098 2877-10286/? D/androidtc: Initializing SystemTextClassifier, type = System
+//    2022-10-09 09:45:53.299 3126-3361/? I/Launcher.NotificationListener: notificationIsValidForUI : com.kakao.talk missingTitleAndText : true isGroupHeader : true
+//    2022-10-09 09:45:53.307 2790-2790/com.example.cd2 I/NotificationListener:  Text:null
+//    2022-10-09 09:45:53.307 2790-2790/com.example.cd2 I/NotificationListener: Sub Text:1개의 안 읽은 메시지
+//    2022-10-09 09:45:53.326 3126-3361/? I/Launcher.NotificationListener: notificationIsValidForUI : com.kakao.talk missingTitleAndText : false isGroupHeader : false
+//    2022-10-09 09:45:53.336 2790-2790/com.example.cd2 I/NotificationListener:  Text:,ㅇㅇ
+//    2022-10-09 09:45:53.336 2790-2790/com.example.cd2 I/NotificationListener: Sub Text:null
+
+//    onNotificationPosted : com.kakao.talk number : 0
+//    2022-10-09 19:14:27.147  3126-3361  Launcher.N...onListener pid-3126                             I  notificationIsValidForUI : com.kakao.talk missingTitleAndText : true isGroupHeader : true
+//    2022-10-09 19:14:27.159 24798-24798 NotificationListener    com.example.cd2                      I   onNotificationPosted() - StatusBarNotification(pkg=com.kakao.talk user=UserHandle{0} id=1 tag=null key=0|com.kakao.talk|1|null|10317: Notification(channel=quiet_new_message shortcut=null contentView=null vibrate=null sound=null defaults=0x0 flags=0x200 color=0xff4d3e36 category=msg groupKey=chat_message vis=PRIVATE semFlags=0x0 semPriority=0 semMissedCount=0))
+//    2022-10-09 19:14:27.159 24798-24798 NotificationListener    com.example.cd2                      I   PackageName:com.kakao.talk
+//    2022-10-09 19:14:27.159 24798-24798 NotificationListener    com.example.cd2                      I   PostTime:1665310466929
+//    2022-10-09 19:14:27.160 24798-24798 NotificationListener    com.example.cd2                      I   Title:null
+//    2022-10-09 19:14:27.160 24798-24798 NotificationListener    com.example.cd2                      I   Text:null
+//    2022-10-09 19:14:27.161 24798-24798 NotificationListener    com.example.cd2                      I  Sub Text:3개의 안 읽은 메시지
+//    2022-10-09 19:14:27.161 24798-24798 NotificationListener    com.example.cd2                      I  Time:1665310466929
+//    2022-10-09 19:14:27.161 24798-24798 NotificationListener    com.example.cd2                      I  Time:UserHandle{0}
+//    2022-10-09 19:14:27.168  3126-3126  Launcher.N...onListener pid-3126                             I  onNotificationPosted : com.kakao.talk number : 3
+//    2022-10-09 19:14:27.169  3126-3361  Launcher.N...onListener pid-3126                             I  notificationIsValidForUI : com.kakao.talk missingTitleAndText : false isGroupHeader : false
+//    2022-10-09 19:14:27.195 24798-24798 NotificationListener    com.example.cd2                      I   onNotificationPosted() - StatusBarNotification(pkg=com.kakao.talk user=UserHandle{0} id=2 tag=100025487382719 key=0|com.kakao.talk|2|100025487382719|10317: Notification(channel=quiet_new_message shortcut=4729f6957d99d466eb0b05f552319a0531a64a8728eab27a73178a0624c85fde contentView=null vibrate=null sound=null tick defaults=0x0 flags=0x10 color=0xff4d3e36 category=msg groupKey=chat_message sortKey=9223370371544309401 actions=2 vis=PRIVATE semFlags=0x0 semPriority=0 semMissedCount=0))
+//    2022-10-09 19:14:27.196 24798-24798 NotificationListener    com.example.cd2                      I   PackageName:com.kakao.talk
+//    2022-10-09 19:14:27.196 24798-24798 NotificationListener    com.example.cd2                      I   PostTime:1665310466942
+//    2022-10-09 19:14:27.196 24798-24798 NotificationListener    com.example.cd2                      I   Title:나
+//    2022-10-09 19:14:27.196 24798-24798 NotificationListener    com.example.cd2                      I   Text:Good Morning
+//    2022-10-09 19:14:27.196 24798-24798 NotificationListener    com.example.cd2                      I  Sub Text:null
+//    2022-10-09 19:14:27.196 24798-24798 NotificationListener    com.example.cd2                      I  Time:1665310466942
+//    2022-10-09 19:14:27.196 24798-24798 NotificationListener    com.example.cd2                      I  Time:UserHandle{0}
+
+//    onNotificationPosted() - StatusBarNotification(pkg=com.samsung.android.messaging user=UserHandle{0} id=123 tag=com.samsung.android.messaging:MESSAGE_RECEIVED:176 key=0|com.samsung.android.messaging|123|com.samsung.android.messaging:MESSAGE_RECEIVED:176|10125: Notification(channel=CHANNEL_ID_SMS_MMS shortcut=7f8701ab82378d31 contentView=null vibrate=null sound=null tick defaults=0x0 flags=0x10 color=0xff3e91ff category=msg groupKey=MESSAGE_RECEIVED sortKey=9223370371543499140 actions=3 vis=PRIVATE semFlags=0x2008 semPriority=0 semMissedCount=1))
+//    2022-10-09 19:27:57.807 25415-25415 NotificationListener    com.example.cd2                      I   PackageName:com.samsung.android.messaging
+//    2022-10-09 19:27:57.807 25415-25415 NotificationListener    com.example.cd2                      I   PostTime:1665311277505
+//    2022-10-09 19:27:57.807 25415-25415 NotificationListener    com.example.cd2                      I   Title:⁨나⁩
+//    2022-10-09 19:27:57.807 25415-25415 NotificationListener    com.example.cd2                      I   Text:집
+//    2022-10-09 19:27:57.808 25415-25415 NotificationListener    com.example.cd2                      I  Sub Text:null
+//    2022-10-09 19:27:57.808 25415-25415 NotificationListener    com.example.cd2                      I  Time:1665311277505
+//    2022-10-09 19:27:57.808 25415-25415 NotificationListener    com.example.cd2                      I  Time:UserHandle{0}
 }
