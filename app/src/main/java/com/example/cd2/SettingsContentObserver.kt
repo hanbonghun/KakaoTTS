bef804e2dbc6f7548d42bbe1d2b2d9c6e10ea534
hanbonghun <bng4535@khu.ac.kr>
2022-11-21 11:19:32 +0900

    kakao api 사용을 위해 androidx 전환

--- app/src/main/java/com/example/cd2/SettingsContentObserver.kt
+++ app/src/main/java/com/example/cd2/SettingsContentObserver.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.database.ContentObserver
 import android.media.AudioManager
 import android.os.Handler
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.SeekBar
 
 
